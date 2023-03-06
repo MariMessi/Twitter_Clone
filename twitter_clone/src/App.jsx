e78060cc6c226cb94a24043354aeb4bdbe401a7b
MariMessi <121039446+MariMessi@users.noreply.github.com>
2023-03-06 19:41:20 +0100

    Add files via upload

---
+++ twitter_clone/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Header from './components/header';
+import PostList from './components/postList';
+import FooterMenu from './components/footerMenu';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+      <PostList />
+      <FooterMenu />
+
+    </div>
+  )
+}
+
+export default App
